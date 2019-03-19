a568ac4e2aa978b34034a912da2a8bd207d926c2
erwan_yifan <1076360205@qq.com>
2019-03-19 18:19:05 +0800

    fastSort method

---
+++ fastSort/src/app/FastFoundK.java
@@ -0,0 +1,17 @@
+package app;
+
+/**
+ * 这里实现一个当初伟冰叫我做的快排查找第K大数字的方法
+ */
+
+public class FastFoundK {
+    public static Array foundK(int[] arr, int k) {
+        
+        
+
+    }
+
+    public static void main(String arg[]) throws Exception {
+        
+    }
+}
